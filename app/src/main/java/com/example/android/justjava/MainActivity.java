af58e0731eb41577ce306657d506957f81b7f35a
zeeman0306 <zeeman0306@gmail.com>
2018-05-24 13:10:49 -0400

    Adding files, second commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.android.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment (View view) {
+        quantity += 1;
+        display(quantity);
+    }
+
+    public void decrement (View view) {
+        quantity -= 1;
+        display(quantity);
+    }
+
+    public void submitOrder (View view) {
+        display(quantity);
+        displayPrice(quantity* 3);
+    }
+
+    private void display (int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
+}
+
